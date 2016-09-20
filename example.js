7f4c0de0c76abc6f36ee6c0e5f4919a03d60c08a
Carlos Landeras Martínez <clanderas@plainconcepts.com>
2016-09-20 14:59:40 +0200

    Remove spaces

--- example.js
+++ example.js
@@ -6,8 +6,7 @@
   
   injector.registerService('userService', userService);
   
-  var foundUser= injector.get('userService').get(1);
-  
+  var foundUser= injector.get('userService').get(1);  
 
   console.log(injector.get('userService').getAll())
   
